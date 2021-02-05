7f71978743595ceb1230340d0545a414c0d867a6
domenkastelich <72886403+domenkastelich@users.noreply.github.com>
2021-02-05 16:31:04 +0000

    Add files via upload

---
+++ simplemoodchecker.py
@@ -0,0 +1,30 @@
+# Just a basic mood checker :)
+
+mood = input("How are you today?: ")
+
+if mood == "happy":
+    print("Wonderfull!")
+
+elif mood == "great":
+    print("That's what i like to hear!")
+
+elif mood == "sad":
+    print("Don't be sad, be happy!")
+
+elif mood == "relaxed":
+    print("What are you smoking? I'm asking for a friend...")
+
+elif mood == "angry":
+    print("Relax, take a chill pill!")
+
+elif mood == "tired":
+    print("Get yourself a RedBull!")
+
+elif mood == "ok":
+    print("Sometimes its better to just be ok.")
+
+elif mood == "good":
+    print("Very good!")
+
+else:
+    print("I don't recognize this mood. Please try again.")
